9c74975aa567ad49b56bd2374c5ab3e15f3ddcc4
Gekeris <you@example.com>
2020-08-21 23:40:47 +0300

    start

---
+++ Slovo/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Slovo
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.IpTextBox = new System.Windows.Forms.TextBox();
+			this.PortTextBox = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.HostButton = new System.Windows.Forms.Button();
+			this.JoinButton = new System.Windows.Forms.Button();
+			this.NameTextBox = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// IpTextBox
+			// 
+			this.IpTextBox.Location = new System.Drawing.Point(52, 56);
+			this.IpTextBox.Name = "IpTextBox";
+			this.IpTextBox.Size = new System.Drawing.Size(156, 20);
+			this.IpTextBox.TabIndex = 0;
+			// 
+			// PortTextBox
+			// 
+			this.PortTextBox.Location = new System.Drawing.Point(52, 82);
+			this.PortTextBox.Name = "PortTextBox";
+			this.PortTextBox.Size = new System.Drawing.Size(156, 20);
+			this.PortTextBox.TabIndex = 1;
+			this.PortTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.PortTextBox_KeyPress);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(29, 60);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(17, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "IP";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(20, 86);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(26, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Port";
+			// 
+			// HostButton
+			// 
+			this.HostButton.Location = new System.Drawing.Point(133, 108);
+			this.HostButton.Name = "HostButton";
+			this.HostButton.Size = new System.Drawing.Size(75, 22);
+			this.HostButton.TabIndex = 4;
+			this.HostButton.Text = "Host";
+			this.HostButton.Click += new System.EventHandler(this.HostButton_Click);
+			// 
+			// JoinButton
+			// 
+			this.JoinButton.Location = new System.Drawing.Point(52, 108);
+			this.JoinButton.Name = "JoinButton";
+			this.JoinButton.Size = new System.Drawing.Size(75, 22);
+			this.JoinButton.TabIndex = 5;
+			this.JoinButton.Text = "Join";
+			// 
+			// NameTextBox
+			// 
+			this.NameTextBox.Location = new System.Drawing.Point(52, 29);
+			this.NameTextBox.Name = "NameTextBox";
+			this.NameTextBox.Size = new System.Drawing.Size(156, 20);
+			this.NameTextBox.TabIndex = 6;
+			this.NameTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.NameTextBox_KeyPress);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(12, 33);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(35, 13);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "Name";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.NameTextBox);
+			this.Controls.Add(this.JoinButton);
+			this.Controls.Add(this.HostButton);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.PortTextBox);
+			this.Controls.Add(this.IpTextBox);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox IpTextBox;
+		private System.Windows.Forms.TextBox PortTextBox;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button HostButton;
+		private System.Windows.Forms.Button JoinButton;
+		private System.Windows.Forms.TextBox NameTextBox;
+		private System.Windows.Forms.Label label3;
+	}
+}
+
---
+++ Slovo/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Slovo
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			PortTextBox.Text = "8000";
+			NameTextBox.Text = "user";
+			//RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(384);
+			//var open = rsa.ExportParameters(true);
+			//var close = rsa.ExportParameters(false);
+
+		}
+
+		private void HostButton_Click(object sender, EventArgs e)
+		{
+			NameTextBox.Text += "@HOST";
+			NameTextBox.ReadOnly = true;
+			NameTextBox.BackColor = Color.White;
+
+			using (WebClient wc = new WebClient())
+			{
+				IpTextBox.Text = wc.DownloadString("https://api.myip.com/").Split('\"')[3];
+			}
+			IpTextBox.ReadOnly = true;
+			IpTextBox.BackColor = Color.White;
+
+			if (PortTextBox.Text == "")
+				PortTextBox.Text = "8000";
+			PortTextBox.ReadOnly = true;
+			PortTextBox.BackColor = Color.White;
+
+			JoinButton.Enabled = false;
+			HostButton.Enabled = false;
+		}
+
+		private void PortTextBox_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			if (!char.IsDigit(e.KeyChar) && (e.KeyChar != 8))
+				e.Handled = true;
+			else
+			{
+				if ((e.KeyChar != 8) && (PortTextBox.Text.Length > 0) && (int.Parse(PortTextBox.Text + e.KeyChar) > 65535))
+				{
+					PortTextBox.Text = "65535";
+					e.Handled = true;
+					PortTextBox.SelectionStart = PortTextBox.Text.Length;
+				}
+			}
+		}
+
+		private void NameTextBox_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			if ((((TextBox) sender).TextLength > 16) || (e.KeyChar == '@'))
+				e.Handled = true;
+		}
+	}
+}
